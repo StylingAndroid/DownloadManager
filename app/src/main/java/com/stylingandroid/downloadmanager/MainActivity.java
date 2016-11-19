4453976ea065fac722ee31135b5a7b4c649f2156
Mark Allison <mark@stylingandroid.com>
2016-11-19 10:54:39 +0000

    Code for Part3

--- app/src/main/java/com/stylingandroid/downloadmanager/MainActivity.java
+++ app/src/main/java/com/stylingandroid/downloadmanager/MainActivity.java
@@ -2,14 +2,26 @@ package com.stylingandroid.downloadmanager;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import static android.Manifest.permission.INTERNET;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 public class MainActivity extends AppCompatActivity implements Downloader.Listener {
     private static final String URI_STRING = "http://www.cbu.edu.zm/downloads/pdf-sample.pdf";
+    private static final String[] PERMISSIONS = new String[]{
+            WRITE_EXTERNAL_STORAGE,
+            READ_EXTERNAL_STORAGE,
+            INTERNET
+    };
 
     private Button download;
 
@@ -18,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements Downloader.Listen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkPermissions();
         setContentView(R.layout.activity_main);
 
         download = (Button) findViewById(R.id.download);
@@ -28,6 +41,15 @@ public class MainActivity extends AppCompatActivity implements Downloader.Listen
             }
         });
         downloader = Downloader.newInstance(this);
+    }
+
+    private void checkPermissions() {
+        for (String permission : PERMISSIONS) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS, 1);
+                return;
+            }
+        }
     }
 
     void downloadOrCancel() {
@@ -46,6 +68,25 @@ public class MainActivity extends AppCompatActivity implements Downloader.Listen
     private void download() {
         Uri uri = Uri.parse(URI_STRING);
         downloader.download(uri);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (!areAllPermissionsGranted(requestCode, grantResults)) {
+            finish();
+        }
+    }
+
+    private boolean areAllPermissionsGranted(int requestCode, @NonNull int[] grantResults) {
+        if (requestCode == 1 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            for (int grantResult : grantResults) {
+                if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
